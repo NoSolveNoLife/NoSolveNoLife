f2d145afd9b14de00a672ca9281421fc91d78b04
wendyunji <wendyunji@gmail.com>
2025-08-03 21:00:29 +0900

    failed: timeout error

---
+++ yunji/250805/BOJ_1325.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[b].append(a)
+
+def bfs(start):
+    q = deque([start])
+    visited = [False]*(n+1)
+    visited[start] = True
+    cnt = 1
+    while q:
+        v = q.popleft()
+        for nv in graph[v]:
+            if not visited[nv]:
+                visited[nv] = True
+                cnt += 1
+                q.append(nv)
+    return cnt
+
+visited_bfs = [0]*(n+1)
+maxv = 0
+for v in range(1, n+1):
+    visited_bfs[v] = bfs(v)
+    if visited_bfs[v] > maxv:
+        maxv = visited_bfs[v]
+
+ans = [str(i) for i in range(1, n+1) if visited_bfs[i] == maxv]
+print(" ".join(ans))
